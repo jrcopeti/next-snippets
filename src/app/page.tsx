c9ab1255a5551ced1c8846d11374db5310e2f033
Jose Copeti <jrcopeti@gmail.com>
2024-02-18 16:01:00 +0100

    edit snippet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import Link from "next/link";
+import { db } from "@/db";
+export default async function Home() {
+  const snippets = await db.snippet.findMany();
 
-export default function Home() {
+  const renderedSnippets = snippets.map((snippet) => {
+    return (
+      <Link
+        href={`/snippets/${snippet.id}`}
+        key={snippet.id}
+        className="flex justify-between items-center p-2 border
+      rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
+  });
   return (
     <div>
-      Home Page
+      <div className="flex m-2 justify-between items-center">
+        <h1 className="text-xl font-bold">Snippets</h1>
+        <Link href="/snippets/new" className="border p-2 rounded">
+          Create a new snippet
+        </Link>
+      </div>
+      <div className="flex flex-col gap-2">{renderedSnippets}</div>
     </div>
-  )
+  );
 }
